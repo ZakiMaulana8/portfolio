dc5d459b42945105bdc26628dfee0786ced85bce
Zaki <jade12052024@gmail.com>
2026-05-09 17:50:56 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence, useScroll, useTransform, useSpring, useInView, useVelocity } from "framer-motion";
 import Lenis from "lenis";
